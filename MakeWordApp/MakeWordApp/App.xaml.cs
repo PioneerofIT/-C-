2f4176a0049246a72d46a1b5f6a58f8895bc9959
PioneerofIT <rjsgml3736@naver.com>
2024-08-25 20:22:27 +0900

    Start Dev

---
+++ MakeWordApp/MakeWordApp/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace MakeWordApp
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
